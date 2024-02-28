2445aa9a83dd936b5f04e85fa2fae661990d482a
Juliakp1 <juliakp710@gmail.com>
2024-02-28 10:08:42 -0300

    feign client start implementation

--- src/main/java/politetransaction/store/account/AccountController.java
+++ src/main/java/politetransaction/store/account/AccountController.java
@@ -1,11 +1,13 @@
 package politetransaction.store.account;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@FeignClient(name = "account")
 public interface AccountController {
 
 	@PostMapping("/accounts")
