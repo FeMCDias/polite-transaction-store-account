1b093509db755e479602a14af4efe206ed43f483
FeMCDias <femcdias@gmail.com>
2024-03-20 09:21:55 -0300

    Add read route

--- src/main/java/politetransaction/store/account/AccountController.java
+++ src/main/java/politetransaction/store/account/AccountController.java
@@ -2,10 +2,12 @@ package politetransaction.store.account;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 
 @FeignClient(name = "store-account")
@@ -27,6 +29,10 @@ public interface AccountController {
 		@RequestBody(required = true) AccountIn in
 	);
 
-
+	@GetMapping("/accounts")
+		public ResponseEntity<AccountOut> read(
+			@RequestHeader(required = true, name = "id-user") String idUser,
+			@RequestHeader(required = true, name = "role-user") String roleUser
+		);
 
 }
